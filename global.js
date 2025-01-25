d72984116d6ec794afaa0848fad53710e851c953
NGLYRY <130323105+NGLYRY@users.noreply.github.com>
2025-01-25 15:55:44 -0800

    debug js

--- global.js
+++ global.js
@@ -1,19 +1,19 @@
 console.log("IT’S ALIVE!");
 
-// Define the $$ function to select multiple elements
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-// Step 2.1: Get an array of all nav links
 const navLinks = $$("nav a");
 
-// Step 2.2: Find the link to the current page
 const currentLink = navLinks.find(
   (a) => a.host === location.host && a.pathname === location.pathname
 );
 
-// Step 2.3: Add the 'current' class to the current page link
 if (currentLink) {
   currentLink.classList.add("current");
 }
+
+
+console.log("Nav Links:", navLinks);
+console.log("Current Link:", currentLink);
